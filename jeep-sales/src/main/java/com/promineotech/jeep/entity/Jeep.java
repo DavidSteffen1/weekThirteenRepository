8a801ea016f6f918b5995204bd2e07d93af03d19
David Steffen <112430270+DavidSteffen1@users.noreply.github.com>
2022-12-10 15:47:18 -0800

    All project files

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -0,0 +1,25 @@
+package com.promineotech.jeep.entity;
+
+import java.math.BigDecimal;
+import lombok.Builder;
+import lombok.Data;
+
+@Data 
+@Builder
+
+public class Jeep {
+
+  private Long modelPK;
+  
+  private JeepModel modelId;
+  
+  private String trimLevel;
+  
+  private int numDoors;
+  
+  private int wheelSize;
+  
+  private BigDecimal basePrice;
+  
+  
+}
